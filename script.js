1d9ac9bc14254dfdec6c17aaea8efe9c7faaacc8
Kyle Luck <luck.kyle@gmail.com>
2016-06-07 11:09:28 -0400

    adding simple directions

--- script.js
+++ script.js
@@ -129,6 +129,46 @@ function initMap() {
   trafficLayer = new google.maps.TrafficLayer();
   toggleTrafficLayer();
 
+  /* directions */
+  var directionsService = new google.maps.DirectionsService(); // no () in documentation
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    draggable: true,
+    map: map,
+    panel: document.getElementById('right-panel')
+  });
+
+  directionsDisplay.addListener('directions_changed', function() {
+    computeTotalDistance(directionsDisplay.getDirections());
+  });
+
+  displayRoute('Atlanta, GA', 'Dunwoody, GA', directionsService, directionsDisplay);
+
+} // end initMap function
+
+function displayRoute(origin, destination, service, display) {
+  service.route({
+    origin: origin,
+    destination: destination,
+    waypoints: [], //what to put here?
+    travelMode: google.maps.TravelMode.DRIVING,
+    avoidTolls: false
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      display.setDirections(response);
+    } else {
+      alert('Could not display directions due to: ' + status);
+    }
+  });
+}
+
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (var i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+  total = total / 1000;
+  document.getElementById('total').innerHTML = total + ' mi';
 }
 
 function toggleTrafficLayer() {
