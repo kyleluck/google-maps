3d2b7f68458a06f472837ca3db15380043076494
Kyle Luck <luck.kyle@gmail.com>
2016-06-07 11:53:48 -0400

    styling changes for directions inputs

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ var showDirections = true;
 
 function initMap() {
 
+  /* variables for directions */
+  var origin_place_id = null;
+  var destination_place_id = null;
+  var travel_mode = google.maps.TravelMode.DRIVING;
+
   //var atlantaLatlng = new google.maps.LatLng(33.749, -84.388);
   var atlantaLatlng = {lat: 33.749, lng: -84.388};
   var atvLatlng = {lat: 33.773887, lng: -84.402853};
@@ -138,11 +143,91 @@ function initMap() {
     panel: document.getElementById('right-panel')
   });
 
-  directionsDisplay.addListener('directions_changed', function() {
-    computeTotalDistance(directionsDisplay.getDirections());
+  var origin_input = document.getElementById('origin-input');
+  var destination_input = document.getElementById('destination-input');
+  var modes = document.getElementById('mode-selector');
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+
+  var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+  origin_autocomplete.bindTo('bounds', map);
+  var destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
+  destination_autocomplete.bindTo('bounds', map);
+
+  // Sets a listener on a radio button to change the filter type on Places Autocomplete.
+  function setupDirectionsClickListener(id, mode) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      travel_mode = mode;
+    });
+  }
+  setupDirectionsClickListener('changemode-walking', google.maps.TravelMode.WALKING);
+  setupDirectionsClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
+  setupDirectionsClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
+
+  function expandViewportToFitPlace(map, place) {
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+  }
+
+  origin_autocomplete.addListener('place_changed', function() {
+    var place = origin_autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+    expandViewportToFitPlace(map, place);
+
+    // If the place has a geometry, store its place ID and route if we have
+    // the other place ID
+    origin_place_id = place.place_id;
+    route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
   });
 
-  displayRoute('Atlanta, GA', 'Dunwoody, GA', directionsService, directionsDisplay);
+  destination_autocomplete.addListener('place_changed', function() {
+    var place = destination_autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+    expandViewportToFitPlace(map, place);
+
+    // If the place has a geometry, store its place ID and route if we have
+    // the other place ID
+    destination_place_id = place.place_id;
+    route(origin_place_id, destination_place_id, travel_mode,
+          directionsService, directionsDisplay);
+  });
+
+  function route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay) {
+    if (!origin_place_id || !destination_place_id) {
+      return;
+    }
+    directionsService.route({
+      origin: {'placeId': origin_place_id},
+      destination: {'placeId': destination_place_id},
+      travelMode: travel_mode
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+
+  // directionsDisplay.addListener('directions_changed', function() {
+  //   computeTotalDistance(directionsDisplay.getDirections());
+  // });
+
+  //displayRoute('Atlanta, GA', 'Dunwoody, GA', directionsService, directionsDisplay);
 
 } // end initMap function
 
